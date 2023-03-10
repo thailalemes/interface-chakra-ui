2ed6e9862b25ba98df5465318dfe66351abbc94c
thailalemes <thaila.lemes01@gmail.com>
2023-03-10 20:32:49 -0300

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.scss';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
